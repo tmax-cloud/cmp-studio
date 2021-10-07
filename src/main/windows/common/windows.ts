b21150692018f92347a11db87a5b5992ddf1c9f9
mjsso <minjin_so@tmax.co.kr>
2021-10-07 17:14:03 +0900

    [feat] WindowService 추가 구현

---
+++ src/main/windows/common/windows.ts
@@ -0,0 +1,27 @@
+import { BrowserWindow } from 'electron';
+
+export interface StudioWindowInterface {
+  readonly id: number;
+  readonly win: BrowserWindow | null /* `null` after being disposed */;
+  setWindowSize(width: number, height: number): void;
+  close(): void;
+}
+
+export const WindowMinimumSize = {
+  WIDTH: 400,
+  WIDTH_WITH_VERTICAL_PANEL: 600,
+  HEIGHT: 270,
+};
+
+export type WindowCreationOptions = {
+  preloadPath: string;
+  state: WindowState;
+};
+
+export type WindowState = {
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
+  display?: number;
+};
---
+++ src/main/windows/electron-main/windowMainService.ts
@@ -0,0 +1,16 @@
+import { ipcMain } from 'electron';
+import { StudioWindowInterface } from '../common/windows';
+export class WindowMainService {
+  constructor(private readonly studioWindow: StudioWindowInterface) {
+    this.registerListeners();
+  }
+
+  private registerListeners() {
+    ipcMain.on(
+      'studio:setWindowSize',
+      (event, arg: { width: number; height: number }) => {
+        this.studioWindow.setWindowSize(arg.width || 100, arg.height || 100);
+      }
+    );
+  }
+}
