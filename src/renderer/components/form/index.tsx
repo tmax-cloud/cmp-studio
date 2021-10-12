3e11d7b94292707b1d429baca8bae938d2b8578b
이경준 <gyoungjun_lee@tmax.co.kr>
2021-10-12 12:29:15 +0900

    [feat] Form Editor 그려주기 이전에 데이터 가공 로직 추가

--- src/renderer/components/form/index.tsx
+++ src/renderer/components/form/index.tsx
@@ -6,51 +6,8 @@ import { ObjectFieldTemplate } from './template/ObjectFieldTemplate';
 import ArrayFieldTemplate from './template/ArrayFieldTemplate';
 import { FieldTemplate } from './template/FieldTemplate';
 
-const setPredefinedData = (title: string): { uiSchema: any; formData: any } => {
-  let predefinedData = { uiSchema: {}, formData: {} };
-  switch (title) {
-    case 'textareaTest':
-      predefinedData = {
-        formData: {
-          analyzer_name: `jsonencode({
-        "Statement" = [{
-          # This policy allows software running on the EC2 instance to
-          # access the S3 API.
-          "Action" = "s3:*",
-          "Effect" = "Allow",
-        }],
-      })`,
-        },
-        uiSchema: {
-          analyzer_name: {
-            'ui:widget': 'textarea',
-            classNames: 'pleasedoitagain',
-          },
-        },
-      };
-      break;
-    case 'resource-aws_iam_policy':
-      predefinedData = {
-        uiSchema: {},
-        formData: {
-          description: 'My test policy',
-          name: 'test_policy',
-          path: '/',
-          policy: `\${jsonencode({\r\n    Version = "2012-10-17"\r\n    Statement = [\r\n      {\r\n        Action = [\r\n          "ec2:Describe*",\r\n        ]\r\n        Effect   = "Allow"\r\n        Resource = "*"\r\n      },\r\n    ]\r\n  })}`,
-        },
-      };
-      break;
-    default:
-  }
-  return predefinedData;
-};
-
 const DynamicForm = (props: FormProps<any>) => {
-  const { fields = {}, schema = {} } = props;
-  const { uiSchema, formData } =
-    setPredefinedData(schema.title as string) || props;
-
-  // const selectSchema = isTestSchemaActivate(testtest, schema);
+  const { fields = {}, schema = {}, formData = {}, uiSchema = {} } = props;
   return (
     <>
       <Form
