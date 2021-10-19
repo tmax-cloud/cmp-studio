a70770a2dae6f3dcaf489e261c421a6975af0415
mjsso <minjin_so@tmax.co.kr>
2021-10-19 16:52:10 +0900

    [refactor] 앱 메타데이타들 관리되는 AppData 폴더 경로를 앱 setup시 생성되는 폴더 하위로 수정

--- src/main/base/common/pathUtils.ts
+++ src/main/base/common/pathUtils.ts
@@ -1,8 +1,22 @@
 import path from 'path';
 import { app } from 'electron';
 
-const PRODUCT_NAME = 'CMPStudio';
+const APP_DATA_PRODUCT_ROOT = 'cmp-studio';
+const PRODUCT_NAME = 'CMP Studio';
 export const SRC_MAIN_PATH = path.join(__dirname, '../../');
+
+const APP_DATA_PRODUCT_ROOT_PATH = path.join(
+  app.getPath('appData'),
+  APP_DATA_PRODUCT_ROOT,
+  PRODUCT_NAME
+);
+
+export function getUserDataFolderPath() {
+  if (app.isPackaged) {
+    return APP_DATA_PRODUCT_ROOT_PATH;
+  }
+  return path.join(__dirname, '../../../../TestMeta');
+}
 
 const RESOURCES_PATH = app.isPackaged
   ? path.join(process.resourcesPath, 'assets')
@@ -12,20 +26,11 @@ export const getAssetPath = (...paths: string[]): string => {
   return path.join(RESOURCES_PATH, ...paths);
 };
 
-const CONFIGS_PATH = app.isPackaged
-  ? path.join(app.getPath('appData'), PRODUCT_NAME, 'Config')
-  : path.join(__dirname, '../../../../TestMeta/Config');
+const CONFIGS_PATH = path.join(getUserDataFolderPath(), 'Config');
 
 export const getConfigsPath = (...paths: string[]): string => {
   return path.join(CONFIGS_PATH, ...paths);
 };
-
-export function getUserDataFolderPath() {
-  if (app.isPackaged) {
-    return path.join(app.getPath('appData'), PRODUCT_NAME);
-  }
-  return path.join(__dirname, '../../../../TestMeta');
-}
 
 export const getWorkspaceMetaFolderPath = () => {
   return path.join(getUserDataFolderPath(), 'Workspaces');
