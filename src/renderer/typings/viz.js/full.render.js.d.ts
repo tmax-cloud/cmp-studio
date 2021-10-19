f0f039f8f921bc5bec5f6995a612e956ace29c8e
yunhee_shin <qlyh8@naver.com>
2021-10-19 17:05:01 +0900

    [refactor] 타입정의 폴더 위치 변경

--- src/renderer/typings/viz.js/full.render.js.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable  @typescript-eslint/ban-types */
-declare module 'viz.js/full.render.js' {
-  export function Module(): any;
-  export function render(instance: any, src: string, options: object): string;
-}
