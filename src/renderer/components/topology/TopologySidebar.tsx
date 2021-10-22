5087eaeafc98df90838eb15481ab5f259f0878cc
이경준 <gyoungjun_lee@tmax.co.kr>
2021-10-22 16:53:23 +0900

    [feat] workspace 변경시에 lnb에도 적용되도록 연동

--- src/renderer/components/topology/TopologySidebar.tsx
+++ src/renderer/components/topology/TopologySidebar.tsx
@@ -163,7 +163,7 @@ const TopologySidebar: React.FC<TopologySidebarProps> = (props) => {
       });
     setItems(itemsList);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [items]);
 
   React.useEffect(() => {
     window.electron.ipcRenderer.on(
