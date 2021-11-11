b6e886a3ec54e685287b6f0b9dc6f0c7492451e8
yunhee_shin <qlyh8@naver.com>
2021-11-11 18:39:48 +0900

    [feat] selectedNode state 추가

--- src/renderer/features/graphSliceInputSelectors.ts
+++ src/renderer/features/graphSliceInputSelectors.ts
@@ -1,5 +1,7 @@
 import { RootState } from '@renderer/app/store';
 
 export const selectGraphData = (state: RootState) => state.graph.graphData;
+export const selectSelectedNode = (state: RootState) =>
+  state.graph.selectedNode;
 export const selectErrorMsg = (state: RootState) => state.graph.errorMsg;
 export const selectLoadingMsg = (state: RootState) => state.graph.loadingMsg;
