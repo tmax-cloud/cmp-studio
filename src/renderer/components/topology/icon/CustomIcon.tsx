aa270fda6f04d10c27f983ea4686a5146ac5ac20
yunhee_shin <qlyh8@naver.com>
2021-11-25 11:11:49 +0900

    아이콘 수정

--- src/renderer/components/topology/icon/CustomIcon.tsx
+++ src/renderer/components/topology/icon/CustomIcon.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const CustomIcon = (props: CustomIconProps) => {
-  const { size, viewBox, color, transform, d } = props;
+  const { useRect, size, viewBox, startColor, endColor, transform, d } = props;
+  const uid = uuidv4();
   return (
     <svg
       width={size}
@@ -9,11 +11,24 @@ const CustomIcon = (props: CustomIconProps) => {
       viewBox={`0 0 ${viewBox} ${viewBox}`}
       xmlns="http://www.w3.org/2000/svg"
     >
+      <defs>
+        <linearGradient x1="0%" y1="100%" x2="100%" y2="0%" id={`lg-${uid}`}>
+          <stop stopColor={startColor} offset="0%" />
+          <stop stopColor={endColor} offset="100%" />
+        </linearGradient>
+      </defs>
       <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g fill={color}>
-          <rect x="0" y="0" width={viewBox || size} height={viewBox || size} />
-        </g>
-        <g transform={transform} fill="#FFFFFF">
+        {useRect ? (
+          <g fill={`url(#lg-${uid})`}>
+            <rect
+              x="0"
+              y="0"
+              width={viewBox || size}
+              height={viewBox || size}
+            />
+          </g>
+        ) : null}
+        <g transform={transform} fill={useRect ? '#FFFFFF' : `url(#lg-${uid})`}>
           <path d={d} />
         </g>
       </g>
@@ -22,15 +37,18 @@ const CustomIcon = (props: CustomIconProps) => {
 };
 
 interface CustomIconProps {
+  useRect?: boolean; // backgrund에 rect 요소를 사용할 지 여부
   size: number;
   viewBox?: number;
-  color: string;
+  startColor: string;
+  endColor: string;
   // eslint-disable-next-line react/require-default-props
   transform?: string;
   d: string;
 }
 
 CustomIcon.defaultProps = {
+  useRect: true,
   viewBox: 24,
 };
 
