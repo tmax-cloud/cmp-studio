c7d3fdee35b9e8db02b6b9e292216ef576e7d82d
이경준 <gyoungjun_lee@tmax.co.kr>
2021-12-21 13:35:30 +0900

    [pr] index.ts에 LoadingModal.tsx export 문 추가

--- src/renderer/components/topology/modal/index.ts
+++ src/renderer/components/topology/modal/index.ts
@@ -1,2 +1,3 @@
 export { default as ModuleListModal } from './ModuleListModal';
 export { default as ModuleImportModal } from './ModuleImportModal';
+export { default as LoadingModal } from './LoadingModal';
